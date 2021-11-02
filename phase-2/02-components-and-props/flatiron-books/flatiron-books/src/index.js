7a34cd13bb3b3809656f3702a855a5cc87d62590
ixnp <enprocopios@gmail.com>
2021-11-02 09:56:21 -0700

    added todos

--- phase-2/02-components-and-props/flatiron-books/flatiron-books/src/index.js
+++ phase-2/02-components-and-props/flatiron-books/flatiron-books/src/index.js
@@ -5,14 +5,3 @@ import App from './App';
 
 
 ReactDOM.render(<App />,document.getElementById('root'));
-
-// const element = <div>Rose is cute</div>
-// console.log(element)
-
-// function App(props){
-//   console.log(props.name)
-//   return(
-//     <div>Hi From {props.name} I am {props.age} years old!!</div>
-//   )
-// }
-// ReactDOM.render(<App name="rose" age="10"/>,document.getElementById('root'));
