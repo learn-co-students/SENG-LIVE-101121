094a6779d5e380eaac75877d178fda1f65f2321c
ixnp <enprocopios@gmail.com>
2021-11-04 10:33:33 -0700

    fixed starter

--- phase-2/04-information-flow/flatiron-books/src/components/App.js
+++ phase-2/04-information-flow/flatiron-books/src/components/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 
 import Header from "./Header";
 import BookContainer from "./BookContainer";
@@ -6,13 +7,14 @@ import Form from "./Form"
 import {books, genres} from '../data/books'
 
 function App() {
+const [bookList, setBookList] = useState(books)
+const [genreList, setGenreList] = useState(genres)
 
   return (
     <div>
       <Header storeName="Barnes and Flatiron" slogan="Live Love Code"/>
       <Form/>
-      <BookContainer bookList={books} genresList={genres} />
- 
+      <BookContainer bookList={bookList} genresList={genreList} />
 
     </div>
   );
--- phase-2/04-information-flow/flatiron-books/src/components/Genre.js
+++ phase-2/04-information-flow/flatiron-books/src/components/Genre.js
@@ -1,5 +1,4 @@
 function Genre({genreStr}){
-    console.log(genreStr)
 
     return(
         <div style={{margin:"5px"}}>{genreStr}</div>
