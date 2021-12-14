1533a2dba4588941fac924288457fef280a69d60
Aysan Isayo <aisayo@icloud.com>
2021-12-14 08:08:27 -0800

    starter code

--- phase-4/02-client-server-comm-1/marketplace/marketplace-api/app/controllers/items_controller.rb
+++ phase-4/02-client-server-comm-1/marketplace/marketplace-api/app/controllers/items_controller.rb
@@ -1,10 +1,16 @@
 class ItemsController < ApplicationController
 
+    # action 
+    # index action 
+    # retreive all the resources 
+
     def index 
         item = Item.all
         render json: item, status: :ok 
     end
 
+
+    # show action: responsible for rendering 1 item with the given id value 
     def show 
         item = Item.find_by(id: params[:id])
         if item 
@@ -14,4 +20,11 @@ class ItemsController < ApplicationController
         end 
     end
 
+    # def show 
+    #     item = Item.find(params[:id])
+    #     render json: item, except: [:created_at, :updated_at], status: :ok 
+
+    # rescue ActiveRecord::RecordNotFound
+    #     render json: {error: "Item not found"}, status: :not_found
+    # end
 end
